f7ff873ca6e4d01b89f1139a8f9c7388440b1ab3
antoine-granier <78824041+antoine-granier@users.noreply.github.com>
2024-02-25 15:48:14 +0100

    fix

--- components/navbar.js
+++ components/navbar.js
@@ -1,18 +1,18 @@
 import Logo from "./logo";
 import NextLink from "next/link"
-import {Container, Box, Link, Stack, Heading, Flex, Menu, MenuItem, MenuList, MenuButton, IconButton, useColorModeValue, Select, Divider} from "@chakra-ui/react";
+import {Container, Box, Stack, Heading, Flex, Menu, MenuItem, MenuList, MenuButton, IconButton, useColorModeValue, Select, Divider} from "@chakra-ui/react";
 import { HamburgerIcon } from "@chakra-ui/icons";
 import ThemeToggleButton from "./theme-toggle-button";
 import { useTranslation } from "react-i18next"
 
-const LinkItem = ({href, path, children}) => {
-    const active = path === href
-    const inactiveColor = useColorModeValue('gray200', 'whiteAlpha.900')
+// const LinkItem = ({href, path, children}) => {
+//     const active = path === href
+//     const inactiveColor = useColorModeValue('gray200', 'whiteAlpha.900')
 
-    return (
-        <Link href={href} p={2} bg={active ? 'glassTeal' : undefined} color={active ? '#202023' : inactiveColor}>{children}</Link>
-    )
-}
+//     return (
+//         <Link href={href} p={2} bg={active ? 'glassTeal' : undefined} color={active ? '#202023' : inactiveColor}>{children}</Link>
+//     )
+// }
 
 const Navbar = props => {
 
