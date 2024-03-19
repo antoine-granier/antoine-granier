6d7ddcac17677a3d7fa7b08949aa553bb20fcadd
antoine-granier <78824041+antoine-granier@users.noreply.github.com>
2024-03-19 23:10:09 +0100

    optimize SEO

--- pages/_document.js
+++ pages/_document.js
@@ -8,12 +8,15 @@ export default class Document extends NextDocument {
             <Html lang='en'>
                 <Head>
                     <title>Antoine Granier - Portfolio</title>
-                    <meta name="description" content="Découvrez le portfolio d'Antoine Granier, jeune développeur web fullstack."/>
+                    <meta name="description" content="Découvrez le portfolio d'Antoine Granier, jeune développeur web fullstack plein d'ambition."/>
+                    <meta name="keywords" content="portfolio, développeur web, développeur web fullstack, web developer, fullstack web developer, developer, développeur, antoine granier, antoine, granier, développeur web paris, développeur web paris, développeur web fullstack paris, developer paris, paris developer, web developer paris, paris web developer, fullstack web developer paris, paris fullstack developer, fullstack"/>
                     <meta property="og:title" content="Antoine Granier - Portfolio" />
                     <meta property="og:type" content="website" />
                     <meta property="og:url" content="https://antoine-granier.vercel.app" />
                     <meta property="og:site_name" content="Antoine Granier - Portfolio" />
                     <meta name="google-site-verification" content="m-IRRKTQ59iJRLIjo62uHVqQEzvie3YdgiDnpszplqY" />
+                    <meta name="robots" content="all" />
+                    <meta name="googlebot" content="all" />
                     <link rel="icon" href="/favicon.ico" sizes="any" />
                 </Head>
                 <body>
